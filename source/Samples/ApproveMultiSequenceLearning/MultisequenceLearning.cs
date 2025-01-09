cc8eefcbe27692529c8e2e551e3aec326b8a8d3d
Singari Sahith Kumar <64829519+sahithkumar1999@users.noreply.github.com>
2025-01-09 21:25:07 +0100

    merge (#3)
    * Approve Multisequence Learning
    
    * Modifying the code
    
    * Modifying the code related to "ApproveMultiSequenceLearning" Project
    
    * Editing the documentation
    
    * Update ApproveMultiSequenceLearning.md
    
    * Bug fixed in SetPermanences. This bug cause lonfer SP learning.
    
    * Scalar Encoder with Buckets
    
    * Update bitmap-representation-of-sdr.md
    
    * review
    
    * AdaptSynapses and JaccardSimilarity
    
    * SDR Reconstruction
    
    * Create Spatial Pooler SDR Reconstruction.md
    
    * renamed method
    
    * 1.1.5
    
    ---------
    
    Co-authored-by: Mahdieh Pirmoradian <mahdieh.pirmoradian@stud.fra-uas.de>
    Co-authored-by: Mahdieh Pirmoradian <74245613+MahdiehPirmoradian@users.noreply.github.com>
    Co-authored-by: Damir Dobric <ddobric@daenet.com>

--- source/Samples/ApproveMultiSequenceLearning/MultisequenceLearning.cs
+++ source/Samples/ApproveMultiSequenceLearning/MultisequenceLearning.cs
@@ -1,16 +1,23 @@
-﻿using NeoCortexApi;
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoCortexApi;
 using NeoCortexApi.Classifiers;
 using NeoCortexApi.Encoders;
 using NeoCortexApi.Entities;
+using NeoCortexApi.Classifiers;
 using NeoCortexApi.Network;
-using Org.BouncyCastle.Asn1.Tsp;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
 
-namespace NeoCortexApiSample
+namespace MultiSequenceLearning
 {
     /// <summary>
     /// Implements an experiment that demonstrates how to learn sequences.
@@ -21,55 +28,16 @@ namespace NeoCortexApiSample
         /// Runs the learning of sequences.
         /// </summary>
         /// <param name="sequences">Dictionary of sequences. KEY is the sewuence name, the VALUE is th elist of element of the sequence.</param>
-        public Predictor Run(Dictionary<string, List<double>> sequences)
+        public Predictor Run(List<Sequence> sequences)
         {
             Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(MultiSequenceLearning)}");
 
             int inputBits = 100;
             int numColumns = 1024;
 
-            HtmConfig cfg = new HtmConfig(new int[] { inputBits }, new int[] { numColumns })
-            {
-                Random = new ThreadSafeRandom(42),
+            HtmConfig cfg = HelperMethods.FetchHTMConfig(inputBits, numColumns);
 
-                CellsPerColumn = 25,
-                GlobalInhibition = true,
-                LocalAreaDensity = -1,
-                NumActiveColumnsPerInhArea = 0.02 * numColumns,
-                PotentialRadius = (int)(0.15 * inputBits),
-                //InhibitionRadius = 15,
-
-                MaxBoost = 10.0,
-                DutyCyclePeriod = 25,
-                MinPctOverlapDutyCycles = 0.75,
-                MaxSynapsesPerSegment = (int)(0.02 * numColumns),
-
-                ActivationThreshold = 15,
-                ConnectedPermanence = 0.5,
-
-                // Learning is slower than forgetting in this case.
-                PermanenceDecrement = 0.25,
-                PermanenceIncrement = 0.15,
-
-                // Used by punishing of segments.
-                PredictedSegmentDecrement = 0.1
-            };
-
-            double max = 255;
-
-            Dictionary<string, object> settings = new Dictionary<string, object>()
-            {
-                { "W", 15},
-                { "N", inputBits},
-                { "Radius", -1.0},
-                { "MinVal", 0.0},
-                { "Periodic", false},
-                { "Name", "scalar"},
-                { "ClipInput", false},
-                { "MaxVal", max}
-            };
-
-            EncoderBase encoder = new ScalarEncoder(settings);
+            EncoderBase encoder = HelperMethods.GetEncoder(inputBits);
 
             return RunExperiment(inputBits, cfg, encoder, sequences);
         }
@@ -77,7 +45,7 @@ namespace NeoCortexApiSample
         /// <summary>
         ///
         /// </summary>
-        private Predictor RunExperiment(int inputBits, HtmConfig cfg, EncoderBase encoder, Dictionary<string, List<double>> sequences)
+        private Predictor RunExperiment(int inputBits, HtmConfig cfg, EncoderBase encoder, List<Sequence> sequences)
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
@@ -95,6 +63,8 @@ namespace NeoCortexApiSample
             CortexLayer<object, object> layer1 = new CortexLayer<object, object>("L1");
 
             TemporalMemory tm = new TemporalMemory();
+
+            Console.WriteLine("------------ START ------------");
 
             // For more information see following paper: https://www.scitepress.org/Papers/2021/103142/103142.pdf
             HomeostaticPlasticityController hpc = new HomeostaticPlasticityController(mem, numUniqueInputs * 150, (isStable, numPatterns, actColAvg, seenInputs) =>
@@ -128,12 +98,12 @@ namespace NeoCortexApiSample
 
             //double[] inputs = inputValues.ToArray();
             int[] prevActiveCols = new int[0];
-            
+
             int cycle = 0;
             int matches = 0;
 
-            var lastPredictedValues = new List<string>(new string[] { "0"});
-            
+            var lastPredictedValues = new List<string>(new string[] { "0" });
+
             int maxCycles = 3500;
 
             //
@@ -146,14 +116,15 @@ namespace NeoCortexApiSample
 
                 cycle++;
 
-                Debug.WriteLine($"-------------- Newborn Cycle {cycle} ---------------");
+                Debug.WriteLine($"-------------- Newborn SP Cycle {cycle} ---------------");
+                Console.WriteLine($"-------------- Newborn SP Cycle {cycle} ---------------");
 
                 foreach (var inputs in sequences)
                 {
-                    foreach (var input in inputs.Value)
+                    foreach (var input in inputs.data)
                     {
-                        Debug.WriteLine($" -- {inputs.Key} - {input} --");
-                    
+                        Debug.WriteLine($" -- {inputs.name} - {input} --");
+
                         var lyrOut = layer1.Compute(input, true);
 
                         if (isInStableState)
@@ -175,16 +146,14 @@ namespace NeoCortexApiSample
             // Loop over all sequences.
             foreach (var sequenceKeyPair in sequences)
             {
-                Debug.WriteLine($"-------------- Sequences {sequenceKeyPair.Key} ---------------");
+                Debug.WriteLine($"-------------- Sequences {sequenceKeyPair.name} ---------------");
+                Console.WriteLine($"-------------- Sequences {sequenceKeyPair.name} ---------------");
 
-                int maxPrevInputs = sequenceKeyPair.Value.Count - 1;
+                int maxPrevInputs = sequenceKeyPair.data.Length - 1;
 
                 List<string> previousInputs = new List<string>();
 
-                previousInputs.Add("-1.0");
-
-                // Set on true if the system has learned the sequence with a maximum acurracy.
-                bool isLearningCompleted = false;
+                previousInputs.Add("-1");
 
                 //
                 // Now training with SP+TM. SP is pretrained on the given input pattern set.
@@ -196,10 +165,10 @@ namespace NeoCortexApiSample
 
                     Debug.WriteLine("");
 
-                    Debug.WriteLine($"-------------- Cycle {cycle} ---------------");
-                    Debug.WriteLine("");
+                    Debug.WriteLine($"-------------- Cycle SP+TM{cycle} ---------------");
+                    Console.WriteLine($"-------------- Cycle SP+TM {cycle} ---------------");
 
-                    foreach (var input in sequenceKeyPair.Value)
+                    foreach (var input in sequenceKeyPair.data)
                     {
                         Debug.WriteLine($"-------------- {input} ---------------");
 
@@ -219,7 +188,7 @@ namespace NeoCortexApiSample
                         if (previousInputs.Count < maxPrevInputs)
                             continue;
 
-                        string key = GetKey(previousInputs, input, sequenceKeyPair.Key);
+                        string key = GetKey(previousInputs, input, sequenceKeyPair.name);
 
                         List<Cell> actCells;
 
@@ -258,21 +227,22 @@ namespace NeoCortexApiSample
                                 Debug.WriteLine($"Current Input: {input} \t| Predicted Input: {item.PredictedInput} - {item.Similarity}");
                             }
 
-                            lastPredictedValues = predictedInputValues.Select(v=>v.PredictedInput).ToList();
+                            lastPredictedValues = predictedInputValues.Select(v => v.PredictedInput).ToList();
                         }
                         else
                         {
                             Debug.WriteLine($"NO CELLS PREDICTED for next cycle.");
-                            lastPredictedValues = new List<string> ();
+                            lastPredictedValues = new List<string>();
                         }
                     }
 
                     // The first element (a single element) in the sequence cannot be predicted
-                    double maxPossibleAccuraccy = (double)((double)sequenceKeyPair.Value.Count - 1) / (double)sequenceKeyPair.Value.Count * 100.0;
+                    double maxPossibleAccuraccy = (double)((double)sequenceKeyPair.data.Length - 1) / (double)sequenceKeyPair.data.Length * 100.0;
 
-                    double accuracy = (double)matches / (double)sequenceKeyPair.Value.Count * 100.0;
+                    double accuracy = (double)matches / (double)sequenceKeyPair.data.Length * 100.0;
 
-                    Debug.WriteLine($"Cycle: {cycle}\tMatches={matches} of {sequenceKeyPair.Value.Count}\t {accuracy}%");
+                    Debug.WriteLine($"Cycle: {cycle}\tMatches={matches} of {sequenceKeyPair.data.Length}\t {accuracy}%");
+                    Console.WriteLine($"Cycle: {cycle}\tMatches={matches} of {sequenceKeyPair.data.Length}\t {accuracy}%");
 
                     if (accuracy >= maxPossibleAccuraccy)
                     {
@@ -284,8 +254,7 @@ namespace NeoCortexApiSample
                         if (maxMatchCnt >= 30)
                         {
                             sw.Stop();
-                            Debug.WriteLine($"Sequence learned. The algorithm is in the stable state after 30 repeats with with accuracy {accuracy} of maximum possible {maxMatchCnt}. Elapsed sequence {sequenceKeyPair.Key} learning time: {sw.Elapsed}.");
-                            isLearningCompleted = true;
+                            Debug.WriteLine($"Sequence learned. The algorithm is in the stable state after 30 repeats with with accuracy {accuracy} of maximum possible {maxMatchCnt}. Elapsed sequence {sequenceKeyPair.name} learning time: {sw.Elapsed}.");
                             break;
                         }
                     }
@@ -298,30 +267,27 @@ namespace NeoCortexApiSample
                     // This resets the learned state, so the first element starts allways from the beginning.
                     tm.Reset(mem);
                 }
-
-                if (isLearningCompleted == false)
-                    throw new Exception($"The system didn't learn with expected acurracy!");
             }
 
             Debug.WriteLine("------------ END ------------");
-           
+
             return new Predictor(layer1, mem, cls);
         }
 
-      
+
         /// <summary>
         /// Gets the number of all unique inputs.
         /// </summary>
         /// <param name="sequences">Alle sequences.</param>
         /// <returns></returns>
-        private int GetNumberOfInputs(Dictionary<string, List<double>> sequences)
+        private int GetNumberOfInputs(List<Sequence> sequences)
         {
             int num = 0;
 
             foreach (var inputs in sequences)
             {
                 //num += inputs.Value.Distinct().Count();
-                num += inputs.Value.Count;
+                num += inputs.data.Length;
             }
 
             return num;
@@ -348,7 +314,7 @@ namespace NeoCortexApiSample
 
                 key += (prevInputs[i]);
             }
-
+            //Console.WriteLine($"GetKey={sequence}_{key}");
             return $"{sequence}_{key}";
         }
     }
